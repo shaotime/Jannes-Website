df8e225bfaa253a03a6394b8f2236ce811c0ef24
shaotime <ericsh@student.chalmers.se>
2018-11-11 03:16:45 +0100

    ContactForm bugs fully fixed
    -Fixed the logic behind submitting forms, now properly works when previously the info wasn't really changing with the state values

--- src/Components/PageComponent/Features/ContactsFeatures/ContactForm.jsx
+++ src/Components/PageComponent/Features/ContactsFeatures/ContactForm.jsx
@@ -4,41 +4,49 @@ class ContactForm extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      nameValue: 'Name',
-      textValue: 'Text'
+      nameValue: '',
+      textValue: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) { //is this actually doing anything???
+  handleChange(event) {
     const target = event.target;
-    const value = target.name === 'nameValue' ? target.nameValue : target.textValue;
+    const value = event.target.value;
     const name = target.name;
 
-    this.setState({[name]: value}); //what is happening here?
+    this.setState({[name]: value});
   }
 
   handleSubmit(event) {
-  //alert('Thank you ' + this.state.nameValue); //this is giving undefined for some reason, figure out why
-    alert('Thank you for your enquiry!');
-    //clear the inputs, setState previously didn't work...fix
+    alert('Thank you ' + this.state.nameValue);
+    this.setState({
+      nameValue: '',
+      textValue: ''
+    });
     event.preventDefault();
+
   }
 
-  render() {//these values aren't actually changing dynamically with the this.state...fix it asap
+  render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <div>
-          Name:
-          <input name="nameValue" type="text" nameValue={this.state.nameValue} onChange={this.handleChange} />
+          <label> Name: </label>
+        </div>
+        <div>
+          <input className="form-control" name="nameValue" type="text" value={this.state.nameValue} onChange={this.handleChange} />
         </div>
         <br />
         <div>
-        Enquiry:
-        <textarea rows="4" cols="50" name="textValue" textValue={this.state.textValue} onChange={this.handleChange} />
+        <label> Enquiry: </label>
         </div>
+        <div>
+        <textarea className="form-control" rows="4" cols="50" name="textValue" value={this.state.textValue} onChange={this.handleChange} />
+        </div>
+        <br />
         <input type="submit" value="Submit" />
       </form>
     );
